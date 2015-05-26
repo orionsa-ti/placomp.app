8f6f050c09fffd9f635723121883c161934e0649
unknown <william.sanches@IT060.orionadm.com.br>
2015-05-26 10:30:45 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+//echo phpinfo();
+
+
+
+$connect = odbc_connect('plactrei','placomp_dba','@orion1!');
+
+if ($connect) {
+    //echo "Connection succeeded.";
+    $result = odbc_tables($connect);
+
+    $i = 1;
+
+	$tables = array();
+	echo "<pre>";
+	while (odbc_fetch_row($result)){
+		if(odbc_result($result,"TABLE_TYPE")=="TABLE"){
+			echo"<br>$i - ".odbc_result($result,"TABLE_NAME");
+			$i++;
+		}
+	}
+	echo "</pre>";
+
+	/*
+	$sql = "SELECT * FROM idcadcli";
+	//$sql = "SELECT endcli FROM idcadcli WHERE guecli LIKE 'ORION%'";
+	$res = odbc_exec($connect, $sql);
+	while( $row = odbc_fetch_array($res) ) { 
+    	echo "<br>";
+    	echo "<pre>";
+    	print_r($row);
+    	echo "</pre>";
+	}
+	*/
+
+}
+else {
+    echo "Connection failed.";
+}
+
+?>
